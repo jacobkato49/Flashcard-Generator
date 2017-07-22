7a2b85dfea6877afef1c39fe0dda1bc17fea84c4
Jacob Kato <jacobkato49@yahoo.com>
2017-07-21 21:31:20 -0700

    finished the switch statement for app.js

--- app.js
+++ app.js
@@ -10,3 +10,60 @@ var fs = require("fs");
 var drawFlashCard;
 var playFlashCard;
 var count= 0;
+
+
+//Option to start with a new flashcard or look at existing ones
+function lookAtMenu(){
+
+  //have inquirer prompt the user
+  inquirer.prompt([
+    //Create an object for the prompt
+    {
+      type: "list",
+      message: "\nPlease chooose the option below.",
+      choices: ["Create New", "Use all", "Random", "Shuffle Please", "Show me the Card", "Exit"],
+      name: "menuOptions"
+    }
+
+  ]).then(function(answer){
+    //create a empty variable and use within the switch to do a setTimeout for each switch function
+    var messageWait;
+
+    //switch statement for the user choice
+    switch(answer.menuOptions){
+
+      case "Create New":
+        console.log("New flashcard time!");
+        messageWait = setTimeout(newCard, 2000);
+        break;
+
+      case "Use all":
+        console.log("These are all the cards you have created");
+        messageWait = setTimeout(quizTime, 1000);
+        break;
+
+      case "Random":
+        console.log("Reading for a random one?");
+        messageWait = setTimeout(randomTime, 2000);
+        break;
+
+      case "Shuffle":
+        console.log("I am shuffling through the deck. One moment please.");
+        messageWait = setTimeout(everdayImShuffling, 3000);
+        break;
+
+      case "Show me the card":
+        console.log("Here you go!");
+        messageWait = setTimeout(reveal, 1000);
+        break;
+
+      case "Exit":
+        console.log("Good luck with your test :)");
+        messageWait = setTimeout(byeBye, 1000);
+        break;
+    }
+  });
+}
+
+//call the function so you can intiate the switch statement
+lookAtMenu();
